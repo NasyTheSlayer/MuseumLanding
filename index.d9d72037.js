3a406ff94bdf8dbaf5676bda45381c07d775059c
NasyTheSlayer <nazar.levkovich.2018@gmail.com>
2025-02-03 15:16:01 +0200

    make build

---
+++ index.d9d72037.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("form"),t=document.getElementById("user-email");e.addEventListener("submit",function(e){e.preventDefault(),t.value=""});
+//# sourceMappingURL=index.d9d72037.js.map
